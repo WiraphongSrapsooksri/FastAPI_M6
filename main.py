80011380013f3c5073f54725778d8fa5385221f3
wiraphong2003 <64011212049@msu.ac.th>
2023-10-01 14:58:59 +0700

    first-commit

---
+++ main.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI
+from PIL import Image, ImageDraw, ImageFont
+import io
+import base64
+import json
+
+app = FastAPI()
+
+@app.get("/draw_image/{number}/{nameH}")
+async def draw_image(number: str,nameH:str):
+    with open('datauser.json', mode='r') as my_file:
+        data = json.load(my_file)
+
+    found = False 
+
+    for user_data in data:
+        if int(number) == int(user_data['cid']):
+            found = True
+            print(True)
+            break 
+
+    if not found:
+        print(False)
+    else:
+        img = Image.open(user_data['imglocal'])
+        draw = ImageDraw.Draw(img)
+
+        font = ImageFont.truetype(user_data['font'], size=76)  
+        # Text to be drawn
+        txt = nameH
+
+        def create_image(message, font):
+            _, _, w, h = draw.textbbox((0, 0), message, font=font)
+            return w,h
+        text_width, text_height = create_image(txt, font)
+        
+        # Position to draw the text
+        position = ((img.width - text_width) // 2, 100)
+
+        # Fill color for the text (black in this example)
+        fill_color = (0, 0, 0)
+
+        # Draw the text with the specified font and size
+        draw.text(position, txt, fill=fill_color, font=font)
+
+        # Save the image
+        # img.save('graph.png')
+
+        # Show the image
+        img.show()
+
+        # Convert the image to a base64 string
+        buffer = io.BytesIO()
+        img.save(buffer, format="PNG")
+        base64_image = base64.b64encode(buffer.getvalue()).decode()
+
+        return {"base64_image": base64_image}
